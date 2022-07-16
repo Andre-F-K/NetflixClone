30c164a89e53c0a892ddd6562349ffd8c1841203
Andre-F-K <98839949+Andre-F-K@users.noreply.github.com>
2022-07-16 20:38:03 +0200

    V0.5
    Finished login (form validate & css)
    Finished Hpme (html and css)
    to do:
    for me page
    link pages together
    add logic to home page

---
+++ scripts/homeScripts.js
@@ -0,0 +1,10 @@
+const nav = document.getElementById('nav')
+
+window.addEventListener('scroll', () =>{
+  if (window.scrollY >=100) {
+    nav.classList.add('navBlack')
+
+  }else{
+    nav.classList.remove('navBlack')
+  }  
+})
---
+++ scripts/loginScript.js
@@ -0,0 +1,50 @@
+
+document.getElementById("formID").addEventListener("submit", function(e){
+        e.preventDefault();
+})
+
+function formValidate() {
+    const userName = document.getElementById('userName').value;
+    const password = document.getElementById('password').value;
+    const passwordConf = document.getElementById('passwordConf').value;
+    console.log(userName)
+     let valid = false
+    
+
+    if (userName === " " || userName === '') {
+        document.getElementById("userNameErrors").innerText = 'username cant be empty'
+        
+    } 
+    else if(userName.length > 15 || userName.length < 6 ) {
+        document.getElementById("userNameErrors").innerText += 'User Name must be between 6 and 15 characters'
+        
+    }
+    else if(password !== passwordConf || password === ''){
+        document.getElementById("userNameErrors").innerText = ''
+        document.getElementById("passwordErrors").innerText = 'Passwords Incorrect'
+        
+    }
+    else{
+        document.getElementById("userNameErrors").innerText = ''
+        document.getElementById("passwordErrors").innerText = '' 
+        return true
+    }
+    valid = true
+    console.log(valid)
+
+    userObj = {
+        name: userName
+    }
+    console.log(userObj)
+    localStorage.setItem('userName', JSON.stringify(userObj))
+
+    
+
+    
+}
+
+function redirect(){
+    window.location.replace("home.html") 
+}
+// window.location.replace("home.html")
+
